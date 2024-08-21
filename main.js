7c8132138ffba9ae55ae7a7cb0f4dcba20dc7804
Gabriel Diniz <gabriel7diniz@outlook.com.br>
2024-08-21 13:58:14 -0300

    algumas repaginadas no html, e criação em processo da criação da div

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 const caminhão = ['ONR5400','230','1','2','3','4','5','6','7','8'];
 const inputPlaca = document.querySelector('.placa');
 const inputKm = document.querySelector('.kmRodado');
+const btnPlaca = document.querySelector('.btplaca')
 
-inputPlaca.addEventListener('click',function(){
+function criaTarefa(textoInput){
+    console.log(textoInput)
+}
+
+function criarDivPneu() {
+
+    const divPneu = document.createElement('div');
+    divPneu.id = 'pneu';
+    const elementosL = ['PNEU', 'KM RODADO:', 'ESTÁGIO DE RECAPAGEM', 'ESTIMATIVA DE DURAÇÃO'];
+    elementosL.forEach(texto => {
+        const span = document.createElement('span');
+        span.textContent = texto;
+        divPneu.appendChild(span);
+        divPneu.appendChild(document.createElement('br'));
+    });
+    const botao = document.createElement('button');
+    botao.textContent = 'ADD RECAPAGEM';
+    botao.style.fontSize = 'xx-small';
+    divPneu.appendChild(botao);
+    return divPneu;
+}
+
+btnPlaca.addEventListener('click',function(){
     if (!inputPlaca.value) return;
-    console.log(inputPlaca.value)
+    criaTarefa(inputPlaca.value);
 });
+
+criarDivPneu();
